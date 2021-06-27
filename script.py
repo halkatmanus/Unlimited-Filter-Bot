311684fa0e8b1bda15a01d1d7da70b8e7eabde9b
Siddharth Raut <66844790+halkatmanus@users.noreply.github.com>
2021-06-27 06:43:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
+    START_MSG = """<b>👋🏻Hey {},
 
 I'm an advanced filter bot with many capabilities!
 There is no practical limits for my filtering capacity :)
@@ -11,50 +11,33 @@ See <i>/help</i> for commands and more details.</b>
 
     HELP_MSG = """
 <i>Add me as admin in your group and start filtering :)</i>
-
-
-<b>Basic Commands;</b>
-
+<b>Basic Commands:</b>
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
 
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
+<b>Filter Commands:</b>
+<code>/add</code> name reply  -  Add filter for name
+<code>/del</code> name  -  Delete filter
 <code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
 <code>/viewfilters</code>  -  List all filters in chat
 
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
+<b>Connection Commands:</b>
+<code>/connect</code> groupid  -  Connect your group to my PM. 
+You can also simply use, <code>/connect</code> in groups.
 <code>/connections</code>  -  Manage your connections.
 
-
 <b>Extras;</b>
-
 /status  -  Shows current status of your bot (Auth User Only)
-
 /id  -  Shows ID information
+<code>/info</code> userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @TroJanzHEX</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Mulgi</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @HalkatManus    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
